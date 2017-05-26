5228e6df333b42ebf21808293e7b2cfc0d1f7712
William Dana <william.b.dana@gmail.com>
2017-05-26 20:03:05 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,44 @@
-# require modules here
+require 'pry'
+require 'yaml'
+emoticons = YAML.load_file('lib/emoticons.yml')
+file_path = 'lib/emoticons.yml'
+emoticon = "☜(⌒▽⌒)☞"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  lib = YAML.load_file(file_path)
+  hash = {
+    "get_emoticon" => {},
+    "get_meaning" => {}
+}
+  lib.each do |m, arr|
+    hash["get_meaning"][arr[1]] = m
+    hash["get_emoticon"][arr[0]] = arr[1]
+  end
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  hash = load_library(file_path)
+  if !hash["get_emoticon"].keys.include?(emoticon)
+    return "Sorry, that emoticon was not found"
+  end
+  hash["get_emoticon"].each do |k, v|
+    if k == emoticon
+      return v
+    end
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  hash = load_library(file_path)
+  if !hash["get_meaning"].keys.include?(emoticon)
+    return "Sorry, that emoticon was not found"
+  end
+  hash["get_meaning"].each do |k, v|
+    if k == emoticon
+      return v
+    end
+  end
 end
+
+get_english_meaning(file_path, emoticon)
